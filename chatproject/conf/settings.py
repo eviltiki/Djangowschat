f711560add1d7ef9a36b6efa80dcc8a4f9fed857
alukyanov <alukyanov@alukyanov.com>
2025-03-18 15:18:42 +0300

    Add req and env files

--- chatproject/conf/settings.py
+++ chatproject/conf/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,11 +21,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=@bg78o9@0zbsyk5g@an=$+yoi(fh-w83&6olt4i(2cnf_a=0z'
+# Инициализация окружения
+env = environ.Env(
+    DEBUG=(bool, False)
+)
+# Чтение .env файла
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+SECRET_KEY = env("SECRET_KEY", default="default-secret-key")
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = ["*"]
 
